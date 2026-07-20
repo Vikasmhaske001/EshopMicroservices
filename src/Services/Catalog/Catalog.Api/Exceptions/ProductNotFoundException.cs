b7e0bcd4d879db33a0e060c352e72c8b233eb6c4
Vikas_Mhaske <vtmhaske@gmail.com>
2026-07-20 10:15:30 +0530

    global exception handling

--- src/Services/Catalog/Catalog.Api/Exceptions/ProductNotFoundException.cs
+++ src/Services/Catalog/Catalog.Api/Exceptions/ProductNotFoundException.cs
@@ -1,9 +1,11 @@
-﻿namespace Catalog.Api.Exceptions
+﻿using BuildingBlocks.Exceptions;
+
+namespace Catalog.Api.Exceptions
 {
-    public class ProductNotFoundException : Exception
+    public class ProductNotFoundException : NotFoundException
     {
-     public ProductNotFoundException(Guid id)
-            : base("Product not found.")
+     public ProductNotFoundException(Guid Id)
+            : base("Product", Id)
         {
 
         }
--- src/Services/Catalog/Catalog.Api/Program.cs
+++ src/Services/Catalog/Catalog.Api/Program.cs
@@ -1,5 +1,3 @@
-using BuildingBlocks.Behaviors;
-
 var builder = WebApplication.CreateBuilder(args);
 
 var assembly = typeof(Program).Assembly;
@@ -18,9 +16,13 @@ builder.Services.AddMarten(opts =>
     opts.Connection(builder.Configuration.GetConnectionString("Database")!);
 }).UseLightweightSessions();
 
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+builder.Services.AddProblemDetails();
+
 var app = builder.Build();
 
 app.MapCarter();
 
+app.UseExceptionHandler();
 
 app.Run();
